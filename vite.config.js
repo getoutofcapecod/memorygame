7137308070094d289a1323f0bf5751a01e2b3997
Austin King <austin@tinycrown.ventures>
2025-06-23 12:23:40 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // This line instructs Vite to build with the correct asset paths
+  base: '/memorygame/', 
   plugins: [react()],
   server: {
     port: 3000,
